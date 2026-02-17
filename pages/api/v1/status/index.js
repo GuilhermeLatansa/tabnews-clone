1c71551339c3aff4977a47ebfbcf0bfc10fcaa94
Guilherme Latansa Barbosa <110869686+GuilhermeLatansa@users.noreply.github.com>
2026-02-17 20:21:56 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
 import database from "/infra/database.js";
+const { getPostgresVersionNumberAsJson } = require("/infra/database.js");
+const { getPostgresMaxConnections } = require("/infra/database.js");
+const { getPostgresUsedConnections } = require("/infra/database.js");
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Aespa goated" });
+  const aespa = "Black Mamba";
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await getPostgresVersionNumberAsJson();
+  const maxConnections = await getPostgresMaxConnections();
+  const usedConnections = await getPostgresUsedConnections();
+
+  response.status(200).json({
+    aespa_goated: aespa,
+    updated_at: updatedAt,
+    postgres_version: postgresVersion,
+    max_connections: parseInt(maxConnections),
+    used_connections: usedConnections,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const parsedPostgresVersion = JSON.parse(responseBody.postgres_version);
+  expect(typeof parsedPostgresVersion).toBe("number");
+
+  expect(responseBody.used_connections).toEqual(1);
+
+  console.log(responseBody);
 });
